0ff30511bc30dcb91e3d2d82092586b45fe703ff
Zohaib0987 <158143584+Zohaib0987@users.noreply.github.com>
2024-12-27 11:53:16 +0000

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'CurrencyConverter.dart'; // Ensure this path is correct
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +10,398 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Currency Converter', // Adding a title for better context
-      home: CurrencyConverter(),
+      title: 'Portfolio',
+      home: PortfolioPage(),
+    );
+  }
+}
+
+class PortfolioPage extends StatelessWidget {
+  const PortfolioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Padding(padding: EdgeInsets.all(10.0)),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Portfolio Interest Section',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  'My Interests:',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Mobile App Development',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Creating innovative and user-friendly mobile applications',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Web Development',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Building responsive and scalable applications',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Machine Learning',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Exploring algorithms and models to extract insights from data',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Cloud Computing',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Leveraging cloud platforms for efficient and scalable cloud solutions',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SecondPage()),
+                  );
+                },
+                child: const Text('Go to Second Screen'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Projects and Experience',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Column(
+            children: [
+              Card(
+                elevation: 2,
+                child: Container(
+                  width: 400,
+                  height: 110,
+                  padding: const EdgeInsets.all(10),
+                  child: const Column(
+                    children: [
+                      Text(
+                        'TodoApp',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 7),
+                      Text(
+                        'A simple todo list app built with flutter and firebase',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Column(
+            children: [
+              Card(
+                elevation: 2,
+                child: Container(
+                  height: 140,
+                  width: 400,
+                  padding: const EdgeInsets.all(10),
+                  child: const Column(
+                    children: [
+                      Text(
+                        'Weather App',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        'A weather App built with flutter which fetches the data from Rest API',
+                        style: TextStyle(fontSize: 18),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Column(
+            children: [
+              Card(
+                elevation: 2,
+                child: Container(
+                  height: 170,
+                  width: 400,
+                  padding: const EdgeInsets.all(10),
+                  child: const Column(
+                    children: [
+                      Text(
+                        'Expense Tracker App',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        'The app enables users to efficiently manage their expenses on-the-go by providing a user-friendly interface to track their spending habits.',
+                        style: TextStyle(fontSize: 18),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Thirdscreen()),
+                      );
+                    },
+                    child: const Text('thirdscreen')),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Thirdscreen extends StatelessWidget {
+  const Thirdscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Column(
+              children: [
+                Center(
+                  child: Text(
+                    'My Portfolio',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  textAlign: TextAlign.right,
+                  'Zohaib   Umer',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.asset(
+              'assets/images/picture2.jpeg',
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 40),
+          const Text(
+            'Zohaib Umer',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'Passionate developer adept at crafting elegant and efficient solutions to complex problems. Dedicated to continuous learning and staying abreast of emerging trends.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                height: 1.5,
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const EducationSection()));
+                  },
+                  child: Center(child: Text('Go to education screen')))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class EducationSection extends StatelessWidget {
+  const EducationSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          _buildInfoRow('Profession', 'Software Developer'),
+          _buildInfoRow('Date Of Birth', '22-06-2004'),
+          _buildInfoRow('Education', 'Higher Education'),
+          const Text(
+            'CHECK RESUME',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoRow(String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Text(
+            '$title: ',
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            value,
+            style: const TextStyle(fontSize: 20),
+          ),
+        ],
+      ),
     );
   }
 }
